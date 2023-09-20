aa3d35ec10653ac88e1bb7dd03ab10c507c9ef64
jeesun 지선 <jeesunikim@users.noreply.github.com>
2023-09-20 08:00:28 -0700

    adding seats to the mint (#5)

--- src/event.rs
+++ src/event.rs
@@ -1,9 +1,4 @@
-use soroban_sdk::{Address, Env, Symbol, symbol_short};
-
-// pub(crate) fn approve(e: &Env, from: Address, to: Address, amount: i128, expiration_ledger: u32) {
-//     let topics = (Symbol::new(e, "approve"), from, to);
-//     e.events().publish(topics, (amount, expiration_ledger));
-// }
+use soroban_sdk::{Address, Env, symbol_short};
 
 pub(crate) fn transfer(e: &Env, from: Address, to: Address, token_id: u32) {
     let topics = (symbol_short!("transfer"), from, to);
@@ -13,11 +8,6 @@ pub(crate) fn transfer(e: &Env, from: Address, to: Address, token_id: u32) {
 pub(crate) fn mint(e: &Env, to: Address, token_id: u32) {
     let topics = (symbol_short!("mint"), to);
     e.events().publish(topics, token_id);
-}
-
-pub(crate) fn set_authorized(e: &Env, owner: Address, id: Address, authorize: bool) {
-    let topics = (Symbol::new(e, "set_authorized"), owner, id);
-    e.events().publish(topics, authorize);
 }
 
 pub(crate) fn set_admin(e: &Env, owner: Address, new_admin: Address) {
