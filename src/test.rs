47f3e82e423c5977082f4b37c7d8e05e604f15d5
jeesun 지선 <jeesunikim@users.noreply.github.com>
2023-09-21 14:12:29 -0700

    add transfer() to the contract (#8)

--- src/test.rs
+++ src/test.rs
@@ -62,6 +62,32 @@ fn test_owner_of() {
 }
 
 #[test]
+fn test_transfer() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let admin = Address::random(&env);
+    let swift_fan_1 = Address::random(&env);
+    let swift_fan_2 = Address::random(&env);
+
+    let eras_token = create_token(&env, &admin);
+
+    eras_token.mint(&swift_fan_1, &1);
+
+    assert_eq!(eras_token.owner_of(&1), swift_fan_1);
+
+    eras_token.transfer(&swift_fan_1, &swift_fan_2, &1);
+
+    assert_eq!(eras_token.owner_of(&1), swift_fan_2);
+
+    eras_token.transfer(&swift_fan_2, &swift_fan_1, &1);
+
+    assert_eq!(eras_token.owner_of(&1), swift_fan_1);
+
+    std::println!("{}", env.logs().all().join("\n"));
+}
+
+#[test]
 #[should_panic(expected = "seat already taken")]
 fn seat_already_taken() {
     let env = Env::default();
@@ -75,7 +101,6 @@ fn seat_already_taken() {
 
     eras_token.mint(&swift_fan_1, &1);
     // assert_eq!(eras_token.balance_of(&swift_fan_1), 1);
-
     eras_token.mint(&swift_fan_2, &1);
 
     std::println!("{}", env.logs().all().join("\n"));
