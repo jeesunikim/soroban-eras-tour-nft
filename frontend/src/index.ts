2196ac3d1f0b3f0dfada5fb93e2f5c56bd6835a3
Jeesun Kim <jeesunkim@Jeesuns-MacBook-Pro.local>
2024-01-17 09:13:20 -0800

    continue frontend

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,10 +1,27 @@
-function component() {
-  const element = document.createElement("div");
+import { isAllowed, setAllowed, getUserInfo } from "@stellar/freighter-api";
 
-  // Lodash, now imported by this script
-  //   element.innerHTML = _.join(["Hello", "webpack"], " ");
+const wrap = document.querySelector("#freighter-wrap");
+const ellipsis = document.querySelector("#freighter-wrap .ellipsis");
+const button = document.querySelector("[data-connect]");
 
-  return element;
+async function getPk() {
+  const { publicKey } = await getUserInfo();
+  return publicKey;
 }
 
-document.body.appendChild(component());
+async function setLoggedIn() {
+  const publicKey = await getPk();
+  ellipsis.innerHTML = `Signed in as ${publicKey}`;
+  ellipsis.title = publicKey;
+}
+
+if (await isAllowed()) {
+  if (await getPk()) setLoggedIn();
+  else wrap.innerHTML = "Freighter is locked.<br>Sign in & refresh the page.";
+} else {
+  button.addEventListener("click", async () => {
+    button.disabled = true;
+    await setAllowed();
+    await setLoggedIn();
+  });
+}
